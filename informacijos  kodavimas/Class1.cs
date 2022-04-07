d8fe254c519153b44a5b50b32245710ab56cf128
erdanilevicius <steamsvabas@gmail.com>
2022-04-07 16:31:05 +0300

    Add project files.

---
+++ informacijos  kodavimas/Class1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace crypt
+{
+ 
+
+    internal class Kodavimas
+    {
+
+        public static string AESSifravimas(string key, string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            File.WriteAllText("AESSifruotas.txt", Convert.ToBase64String(array));
+            return Convert.ToBase64String(array);
+        }
+        /// /////////////////////////////////////////////////
+
+        public static string SSifravimas(string key, string TextToEncrypt)
+        {
+            byte[] MyEncryptedArray = UTF8Encoding.UTF8.GetBytes(TextToEncrypt);
+
+            MD5CryptoServiceProvider MyMD5CryptoService = new MD5CryptoServiceProvider();
+
+            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash (UTF8Encoding.UTF8.GetBytes(key));
+
+            MyMD5CryptoService.Clear();
+
+            var MyTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+
+            MyTripleDESCryptoService.Key = MysecurityKeyArray;
+
+            MyTripleDESCryptoService.Mode = CipherMode.ECB;
+
+            MyTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var MyCrytpoTransform = MyTripleDESCryptoService.CreateEncryptor();
+
+            byte[] MyresultArray = MyCrytpoTransform.TransformFinalBlock(MyEncryptedArray, 0, MyEncryptedArray.Length);
+
+            MyTripleDESCryptoService.Clear();
+
+            File.WriteAllText("3DESSifruotas.txt", Convert.ToBase64String(MyresultArray, 0, MyresultArray.Length));
+            return Convert.ToBase64String(MyresultArray, 0,MyresultArray.Length);
+        }
+
+
+
+
+
+        ///////////////////////////////////////////////////////
+        public static string AESDesifravimas(string key, string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+        ////////////////////////////////////////////////////
+
+        public static string SDesifravimas(string key, string TextToDecrypt)
+        {
+            byte[] MyDecryptArray = Convert.FromBase64String(TextToDecrypt);
+
+            MD5CryptoServiceProvider MyMD5CryptoService = new MD5CryptoServiceProvider();
+
+            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+
+            MyMD5CryptoService.Clear();
+
+            var MyTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+
+            MyTripleDESCryptoService.Key = MysecurityKeyArray;
+
+            MyTripleDESCryptoService.Mode = CipherMode.ECB;
+
+            MyTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var MyCrytpoTransform = MyTripleDESCryptoService.CreateDecryptor();
+
+            byte[] MyresultArray = MyCrytpoTransform.TransformFinalBlock(MyDecryptArray, 0, MyDecryptArray.Length);
+
+            MyTripleDESCryptoService.Clear();
+
+            File.WriteAllText("3DESDesifruotas.txt", UTF8Encoding.UTF8.GetString(MyresultArray));
+            return UTF8Encoding.UTF8.GetString(MyresultArray);
+        }
+
+
+    }
+}
---
+++ informacijos  kodavimas/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace crypt
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+            //Console.WriteLine("Please enter a secret key for the symmetric algorithm.");  
+            //var key = Console.ReadLine();  
+
+
+            while (i != 1) {
+                Console.WriteLine("Pasirinkite ka norite daryti:");
+                Console.WriteLine("1 - uzsifruoti teksta");
+                Console.WriteLine("2 - desifruoti teksta");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    int j = 0;
+
+                    while(j != 1) {
+
+                        Console.Clear();
+                        Console.WriteLine("pasirinkite sifravimo metoda");
+                        Console.WriteLine("1 - AES");
+                        Console.WriteLine("2 - 3DES");
+                        string pasirinkimas = Console.ReadLine();
+                        if (pasirinkimas == "1")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Irasykite ka norite uzsifruoti: ");
+                            var str = Console.ReadLine();
+                            Console.Clear();
+                            var sifruojamas = Kodavimas.AESSifravimas(key, str);
+                            Console.WriteLine($"uzsifruotas tekstas = {sifruojamas}");
+                            j = 1;
+                        }
+                        if (pasirinkimas == "2")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Irasykite ka norite uzsifruoti: ");
+                            var tekstas = Console.ReadLine();
+                            Console.Clear();
+                            var sifruoju = Kodavimas.SSifravimas(key, tekstas);
+                            Console.WriteLine($"uzsifruotas tekstas = {sifruoju}");
+                            j = 1;
+
+                        }
+                        else if (pasirinkimas != "1" && pasirinkimas != "2")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Irasykite teisinga pasirinkima");
+                            
+                        }
+
+                    }
+
+                    i = 1;
+
+                }
+                if (choice == "2")
+                {
+                    int j = 0;
+
+                    while (j != 1) {
+                        Console.Clear();
+                        Console.WriteLine("pasirinkite sifravimo metoda");
+                        Console.WriteLine("1 - AES");
+                        Console.WriteLine("2 - 3DES");
+                        string pasirinkimas = Console.ReadLine();
+
+                        if (pasirinkimas == "1") {
+
+                            Console.Clear();
+                            Console.WriteLine("Irasykite teksta kuri norite desifruoti");
+                            string sifruotas = Console.ReadLine();
+                            Console.Clear();
+                            var desifruojamas = Kodavimas.AESDesifravimas(key, sifruotas);
+                            Console.WriteLine($"decrypted string = {desifruojamas}");
+                            Console.ReadKey();
+                            File.WriteAllText("Desifruotas.txt", desifruojamas);
+                            j = 1;
+
+                        }
+
+                        if (pasirinkimas == "2")
+                        {
+
+                            Console.Clear();
+                            Console.WriteLine("Irasykite ka norite desifruoti: ");
+                            var tekstas = Console.ReadLine();
+                            Console.Clear();
+                            var sifruoju = Kodavimas.SDesifravimas(key, tekstas);
+                            Console.WriteLine($"uzsifruotas tekstas = {sifruoju}");
+                            j = 1;
+
+                        }
+                        else if (pasirinkimas != "1" && pasirinkimas != "2") {
+                            Console.Clear();
+                            Console.WriteLine("Irasykite teisinga pasirinkima");
+                        }
+
+                    }   
+
+                    i = 1;
+
+                }
+                else if(choice != "1" && choice != "2")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Irasykite teisinga pasirinkima");
+                }
+
+            }
+
+            while (true) ;
+
+        }
+    }
+}
+
